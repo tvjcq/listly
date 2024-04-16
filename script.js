885c869534036a4dff016ebe45c431a33f6c21c5
bobby <deter.eliam@gmail.com>
2024-04-16 23:41:01 +0200

    add local storage

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTasks = localStorage.getItem("tasks");
+  if (savedTasks) {
+    const tasks = JSON.parse(savedTasks);
+
+    const taskList = document.getElementById("taskList");
+    tasks.forEach((task) => {
+      const newTask = document.createElement("li");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+          newTask.style.textDecoration = "line-through";
+        } else {
+          newTask.style.textDecoration = "none";
+        }
+      });
+
+      newTask.textContent = task;
+      newTask.appendChild(checkbox);
+      taskList.appendChild(newTask);
+    });
+  }
+});
+
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", function () {
+  localStorage.removeItem("tasks");
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+});
+
+const buttonAddTask = document.getElementById("buttonAddTask");
+buttonAddTask.addEventListener("click", addTask);
+
+const taskInput = document.getElementById("taskInput");
+taskInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
 function addTask() {
   const taskInput = document.getElementById("taskInput");
   const taskList = document.getElementById("taskList");
@@ -18,10 +60,24 @@ function addTask() {
     newTask.appendChild(checkbox);
     taskList.appendChild(newTask);
     taskInput.value = "";
+
+    saveTasksToLocalStorage();
   }
 }
 
-const taskInput = document.getElementById("taskInput");
+function saveTasksToLocalStorage() {
+  const tasks = [];
+
+  // Récupérer toutes les tâches de la liste
+  const taskItems = document.querySelectorAll("#taskList li");
+  taskItems.forEach((task) => {
+    tasks.push(task.textContent);
+  });
+
+  // Enregistrer les tâches au format JSON dans le stockage local
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 const underlineInput = document.getElementById("underlineInput");
 
 taskInput.addEventListener("focus", function () {
@@ -29,5 +85,7 @@ taskInput.addEventListener("focus", function () {
 });
 
 taskInput.addEventListener("blur", function () {
-  underlineInput.style.width = "480px";
+  if (taskInput.value === "") {
+    underlineInput.style.width = "480px";
+  }
 });
