cca2eb7dad596198c9d47b2894b3174d3335543e
bobby <deter.eliam@gmail.com>
2024-04-19 23:36:28 +0200

    Set local storage for finished tasks

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 // Récupérer les données du stockage local
 document.addEventListener("DOMContentLoaded", () => {
   const savedTasks = localStorage.getItem("tasks");
+  const savedTasksDone = localStorage.getItem("tasksDone");
+  var delayIndex = 0;
   if (savedTasks) {
     const tasks = JSON.parse(savedTasks);
 
     const taskList = document.getElementById("taskList");
     tasks.forEach((task, index) => {
-      // TODO : créer une fonction pour ajouter une tâche à la liste
+      console.log("marqueur 2");
       const newTask = document.createElement("li");
-      const checkbox = document.createElement("input");
+      const text = document.createElement("p");
+      const checkbox = document.createElement("button");
       const line = document.createElement("div");
+      const strike = document.createElement("div");
+      newTask.className = "task";
       line.className = "line";
-      checkbox.type = "checkbox";
-      checkbox.addEventListener("change", function () {
-        if (checkbox.checked) {
-          newTask.style.textDecoration = "line-through";
+      strike.className = "line";
+      strike.id = "strike";
+      checkbox.textContent = "Fait";
+      checkbox.addEventListener("click", function () {
+        if (newTask.classList.contains("task")) {
+          text.animate([{ opacity: 1 }, { opacity: 0.5 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          strike.animate([{ width: "0%" }, { width: "80%" }], {
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-in-out",
+          });
+          strike.animate([{ opacity: 1 }, { opacity: 0.5 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          checkbox.textContent = "Annuler";
+          newTask.classList.remove("task");
+          newTask.classList.add("taskDone");
         } else {
-          newTask.style.textDecoration = "none";
+          text.animate([{ opacity: 0.5 }, { opacity: 1 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          strike.animate([{ width: "80%" }, { width: "0%" }], {
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-in-out",
+          });
+          strike.animate([{ opacity: 0.5 }, { opacity: 1 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          checkbox.textContent = "Fait";
+          newTask.classList.remove("taskDone");
+          newTask.classList.add("task");
         }
       });
       newTask.style.opacity = 0;
@@ -41,9 +78,115 @@ document.addEventListener("DOMContentLoaded", () => {
         taskList.appendChild(line);
       }
 
-      newTask.textContent = task;
+      text.textContent = task;
+      newTask.appendChild(text);
       newTask.appendChild(checkbox);
       taskList.appendChild(newTask);
+      taskList.appendChild(strike);
+
+      delayIndex++;
+    });
+  }
+  if (savedTasksDone) {
+    const task = JSON.parse(savedTasksDone);
+    const taskList = document.getElementById("taskChecked");
+
+    task.forEach((task, index) => {
+      console.log("marqueur 1");
+      const newTask = document.createElement("li");
+      const text = document.createElement("p");
+      const checkbox = document.createElement("button");
+      const line = document.createElement("div");
+      const strike = document.createElement("div");
+      newTask.className = "taskDone";
+      line.className = "line";
+      strike.className = "line";
+      strike.id = "strike";
+      checkbox.textContent = "Rétablir";
+      checkbox.addEventListener("click", function () {
+        if (newTask.classList.contains("task")) {
+          text.animate([{ opacity: 1 }, { opacity: 0.5 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          strike.animate([{ width: "0%" }, { width: "80%" }], {
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-in-out",
+          });
+          strike.animate([{ opacity: 1 }, { opacity: 0.5 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          checkbox.textContent = "Annuler";
+          newTask.classList.remove("task");
+          newTask.classList.add("taskDone");
+        } else {
+          text.animate([{ opacity: 0.5 }, { opacity: 1 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          strike.animate([{ width: "80%" }, { width: "0%" }], {
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-in-out",
+          });
+          strike.animate([{ opacity: 0.5 }, { opacity: 1 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+          checkbox.textContent = "Fait";
+          newTask.classList.remove("taskDone");
+          newTask.classList.add("task");
+        }
+      });
+
+      newTask.style.opacity = 0;
+
+      newTask.animate([{ opacity: 0 }, { opacity: 1 }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: delayIndex * 300,
+      });
+      newTask.animate(
+        [{ transform: "translateX(-5%)" }, { transform: "translateX(0%)" }],
+        {
+          duration: 1000,
+          fill: "forwards",
+          easing: "ease",
+          delay: delayIndex * 300,
+        }
+      );
+      line.style.width = "0%";
+      line.animate([{ width: "0%" }, { width: "100%" }], {
+        duration: 1000,
+        fill: "forwards",
+        easing: "ease-in-out",
+        delay: delayIndex * 300,
+      });
+      text.animate([{ opacity: 0 }, { opacity: 0.5 }], {
+        duration: 1000,
+        fill: "forwards",
+        delay: delayIndex * 300,
+      });
+      strike.animate([{ width: "0%" }, { width: "80%" }], {
+        duration: 1000,
+        fill: "forwards",
+        easing: "ease-in-out",
+        delay: delayIndex * 300,
+      });
+
+      if (index !== 0) {
+        taskList.appendChild(line);
+      }
+
+      text.textContent = task;
+      newTask.appendChild(text);
+      newTask.appendChild(checkbox);
+      taskList.appendChild(newTask);
+      taskList.appendChild(strike);
+
+      delayIndex++;
     });
   }
 });
@@ -55,8 +198,11 @@ document.addEventListener("DOMContentLoaded", () => {
 const resetButton = document.getElementById("resetButton");
 resetButton.addEventListener("click", function () {
   localStorage.removeItem("tasks");
+  localStorage.removeItem("tasksDone");
   const taskList = document.getElementById("taskList");
+  const taskChecked = document.getElementById("taskChecked");
   taskList.innerHTML = "";
+  taskChecked.innerHTML = "";
 });
 
 // Ajouter une tâche à la liste en appuyant sur le bouton "Ajouter"
@@ -85,9 +231,8 @@ function addTask() {
     line.className = "line";
     strike.className = "line";
     strike.id = "strike";
-    console.log("marque");
     checkbox.textContent = "Fait";
-    // TODO : Faire en sort que le li aille dans le ul des taches terminées
+    // TODO : Après un certain temps, la tache vas dans les tâches terminées
     checkbox.addEventListener("click", function () {
       if (newTask.classList.contains("task")) {
         text.animate([{ opacity: 1 }, { opacity: 0.5 }], {
@@ -161,13 +306,24 @@ function saveTasksToLocalStorage() {
   const tasks = [];
   const tasksDone = [];
 
-  // Récupérer toutes les tâches de la liste
   const taskItems = document.querySelectorAll("#taskList li");
+  const taskCheckedItems = document.querySelectorAll("#taskChecked li");
+
   taskItems.forEach((task) => {
+    const taskText = task.querySelector("p").textContent;
     if (task.classList.contains("task")) {
-      tasks.push(task.textContent);
+      tasks.push(taskText);
     } else {
-      tasksDone.push(task.textContent);
+      tasksDone.push(taskText);
+    }
+  });
+
+  taskCheckedItems.forEach((task) => {
+    const taskText = task.querySelector("p").textContent;
+    if (task.classList.contains("task")) {
+      tasks.push(taskText);
+    } else {
+      tasksDone.push(taskText);
     }
   });
 
